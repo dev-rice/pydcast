9e1557c14238f947264c9b76e6d5cfbf360132c6
Chris Rice <crice@mines.edu>
2013-12-18 15:57:31 -0700

    Forwent curses for a simple number input selection

--- curses_test.py
+++ curses_test.py
@@ -6,6 +6,17 @@ curses.noecho()
 curses.cbreak()
 stdscr.keypad(True)
 
+file = open("podcast_list.txt", "r")
+
+y = 0
+names = []
+links = []
+for line in file:
+	names.append(line.split(",")[0])
+	links.append(line.split(",")[1])
+	
+podcasts = dict.fromkeys(names, links)
+
 while True:
     c = stdscr.getch()
 
@@ -23,3 +34,6 @@ curses.nocbreak()
 stdscr.keypad(False)
 curses.echo()
 curses.endwin()
+
+print(names)
+print(links)
